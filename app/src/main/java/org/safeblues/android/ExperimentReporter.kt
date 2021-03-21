0a826667da144296c15a79ef815640a1c61cf404
Aapeli <git@aapelivuorinen.com>
2021-03-20 20:02:45 -0400

    Use JSON over HTTP for PMS instead of gRPC

--- app/src/main/java/org/safeblues/android/ExperimentReporter.kt
+++ app/src/main/java/org/safeblues/android/ExperimentReporter.kt
@@ -4,27 +4,21 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.util.Log
-import com.google.android.gms.location.Geofence
-import com.google.android.gms.location.GeofenceStatusCodes
-import com.google.android.gms.location.GeofencingEvent
-import io.grpc.ManagedChannelBuilder
-import org.safeblues.api.SafeBluesProtos
-import org.safeblues.android.persistence.*
-import org.safeblues.api.ExperimentAPIGrpcKt
-import java.lang.Exception
-import com.google.protobuf.util.Timestamps.fromMillis
+import com.google.gson.Gson
 import io.bluetrace.opentrace.Preference
 import io.bluetrace.opentrace.Utils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Request
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.safeblues.android.persistence.ExperimentDatabase
 import pub.devrel.easypermissions.EasyPermissions
 
 
 object ExperimentReporter {
     private val TAG = "SB_XPR"
-
-    private fun getStub(): ExperimentAPIGrpcKt.ExperimentAPICoroutineStub {
-        val channel = ManagedChannelBuilder.forAddress("xp-api.safeblues.org", 8443).useTransportSecurity().build()
-        return ExperimentAPIGrpcKt.ExperimentAPICoroutineStub(channel)
-    }
 
     // naughty naughtyyy
     private fun isActiveForExperiment(context: Context): Boolean {
@@ -57,24 +51,43 @@ object ExperimentReporter {
 
     suspend fun pushExperimentStatus(context: Context): Boolean {
         try {
-            val stub = getStub()
             val experimentDao = ExperimentDatabase.getDatabase(context).experimentDao()
 
-            val ret = SafeBluesProtos.ReportStatusRequest.newBuilder()
-                .setParticipantId(Preference.getParticipantId(context))
-
+            val data = ExperimentalDataJson()
+            data.participant_id = Preference.getParticipantId(context)
             for (entry in experimentDao.getUnsentExperimentData()) {
-                ret.addStatuses(SafeBluesProtos.PhoneStatus.newBuilder().apply{
-                    // exit_time is guaranteed to be non-null here due to spec in sql query
-                    this.duration = (((entry.exit_time ?: 0) - entry.enter_time) / (1000*60*15)).toDouble()
-                    this.truncatedEntry = fromMillis(entry.enter_time / 86400000)
-                    this.countActive = entry.count_active
-                })
+                val status = ExperimentalDataStatus()
+                status.status_id = entry.id
+                status.duration = (((entry.exit_time ?: 0) - entry.enter_time) / (1000 * 60 * 15))
+                status.count_active = entry.count_active
+                status.truncate_entry_time = (entry.enter_time / 86400000)
+                data.statuses.add(status)
             }
-            val report = ret.build()
-            stub.reportStatus(report)
-            experimentDao.markAllAsSent()
-            return true
+
+            val json = Gson().toJson(data)
+
+            Log.d(TAG, "JSON about to be sent to PMS:")
+            Log.d(TAG, json)
+
+            val client = OkHttpClient()
+
+            val request: Request = Request.Builder()
+                .url("https://participant.safeblues.org/api/push_experiment_data")
+                .post(json.toRequestBody("application/json; charset=utf-8".toMediaType()))
+                .build()
+
+            Log.d(TAG, "About to do that HTTP thing")
+            val result = withContext(Dispatchers.IO) { client.newCall(request).execute()}
+            if (result.isSuccessful) {
+                Log.d(TAG, "Successfully HTTPeed")
+                Log.d(TAG, result.body.toString())
+                experimentDao.markAllAsSent()
+                return true
+            } else {
+                Log.d(TAG, "Failed to HTTPee")
+                Log.d(TAG, result.message)
+                return false
+            }
         } catch (e: Exception) {
             Log.e(TAG, "Failed to send experimental report: " + e.toString())
             return false
---
+++ app/src/main/java/org/safeblues/android/ExperimentalDataJson.kt
@@ -0,0 +1,14 @@
+package org.safeblues.android
+
+
+class ExperimentalDataStatus {
+    var status_id: Int = 0
+    var truncate_entry_time: Long = 0
+    var duration: Long = 0
+    var count_active: Long = 0
+}
+
+class ExperimentalDataJson {
+    var participant_id: String = ""
+    var statuses: MutableList<ExperimentalDataStatus> = ArrayList()
+}
